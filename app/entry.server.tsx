608fa6ad98b152524c1bbe585f8807a9f70deb0c
tohachan <toha.chan@gmail.com>
2025-06-06 11:42:44 +0700

    Fix dependencies

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,6 @@
-import { RemixServer } from '@remix-run/react'
-import { renderToReadableStream } from 'react-dom/server'
-import type { EntryContext } from '@remix-run/node'
+import { RemixServer } from '@remix-run/react';
+import { renderToString } from 'react-dom/server';
+import type { EntryContext } from '@remix-run/node';
 
 export default function handleRequest(
   request: Request,
@@ -8,11 +8,22 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  return renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} />,
-    {
-      status: responseStatusCode,
-      headers: responseHeaders
+  const html = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  );
+  
+  const body = new ReadableStream({
+    start(controller) {
+      controller.enqueue(new TextEncoder().encode('<!DOCTYPE html>' + html));
+      controller.close();
+    },
+  });
+  
+  return new Response(body, {
+    status: responseStatusCode,
+    headers: {
+      ...Object.fromEntries(responseHeaders),
+      'Content-Type': 'text/html',
     }
-  )
+  });
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import remix from '@remix-run/vite'
+import { vitePlugin as remix } from '@remix-run/dev'
 
 export default defineConfig({
   plugins: [remix()],
